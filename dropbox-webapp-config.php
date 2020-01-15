61906661b31862d7feb7a12d7d45755589cd1eaf
dval <dylan.valentine@gmail.com>
2020-01-15 11:15:51 -0500

    Added config file before ignoring the changes

---
+++ dropbox-webapp-config.php
@@ -0,0 +1,20 @@
+<?php 
+/* dropbox-webapp-config.php */
+/* 
+ * Use this file to store your Dropbox application credentials.
+ * You can get these when you set up your app at Dropbox.com.
+ *
+ */
+
+namespace gamesolids;
+
+$app_config = array();
+
+//
+$app_config['app_key'] = " ";
+
+$app_config['app_secret'] = " ";
+
+$app_config['access_token'] = " ";
+
+?>
